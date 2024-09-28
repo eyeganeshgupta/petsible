633662461db2c395b430b7cc69e31196232e83a2
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-09-28 23:36:44 +0530

    setting-up-development-environment

---
+++ src/main/java/com/petsible/PetsibleApplication.java
@@ -0,0 +1,11 @@
+package com.petsible;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetsibleApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(PetsibleApplication.class, args);
+	}
+}
---
+++ src/test/java/com/petsible/PetsibleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petsible;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetsibleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
